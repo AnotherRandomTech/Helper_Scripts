6ccc46ae1578202cf515d597c4be64010a9b60b8
Luke <31136246+AnotherRandomTech@users.noreply.github.com>
2018-12-20 18:15:50 +0800

    Update Random Audio Prank.ps1
    added set-volume to 100%

--- Powershell - General/pranks/Random Audio Prank.ps1
+++ Powershell - General/pranks/Random Audio Prank.ps1
@@ -28,6 +28,12 @@ Write-Error "Error downloading prank audio!"
 exit -1
 }
 
+## Set Audio to 100%
+Function Set-Speaker($Volume){
+$wshShell = new-object -com wscript.shell;1..50 | % {$wshShell.SendKeys([char]174)};1..$Volume | % {$wshShell.SendKeys([char]175)}
+}
+Set-Speaker -Volume 50
+
 Add-Type -AssemblyName presentationCore
 $filepath = [uri] ($env:APPDATA + ".\prank_audio.mp3")
 
